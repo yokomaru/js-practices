9e93530bb583b2c1a1fb65628ff076cac1ce38f2
yokomaru <cabochetoganlan@gmail.com>
2024-10-23 23:36:45 +0900

    delete: todayYear and todayMonth
    - 上記変数は一度しか使っていないため、変数を使用しないように修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -5,8 +5,6 @@ import chalk from "chalk";
 
 const argv = minimist(process.argv.slice(2));
 const today = new Date();
-const todayYear = today.getFullYear();
-const todayMonth = today.getMonth() + 1;
 const todayString = today.toLocaleDateString("ja-JP", {
   year: "numeric",
   month: "2-digit",
@@ -15,54 +13,53 @@ const todayString = today.toLocaleDateString("ja-JP", {
 const year =
   typeof argv["y"] !== "undefined" && 1970 <= argv["y"] && argv["y"] <= 2100
     ? argv["y"]
-    : todayYear;
+    : today.getFullYear();
 const month =
   typeof argv["m"] !== "undefined" && 1 <= argv["m"] && argv["m"] <= 12
     ? argv["m"]
-    : todayMonth;
+    : today.getMonth() + 1;
+    const lastMonthDate = new Date(year, month, 1);
 
-const lastMonthDate = new Date(year, month, 1);
+    const firstMonthDate = new Date(year, month - 1, 1);
+    const firstMonthDay = new Date(firstMonthDate).getDay();
 
-const firstMonthDate = new Date(year, month - 1, 1);
-const firstMonthDay = new Date(firstMonthDate).getDay();
+    const calenderTitle = `${month}月 ${year}`;
+    const calenderTitleLength = calenderTitle.length;
 
-const calenderTitle = `${month}月 ${year}`;
-const calenderTitleLength = calenderTitle.length;
+    console.log(
+      `${calenderTitle
+        .padStart(
+          calenderTitleLength + Math.floor((20 - calenderTitleLength) / 2),
+          " ",
+        )
+        .padEnd(20, " ")}  `,
+    );
+    console.log("日 月 火 水 木 金 土  ");
 
-console.log(
-  `${calenderTitle
-    .padStart(
-      calenderTitleLength + Math.floor((20 - calenderTitleLength) / 2),
-      " ",
-    )
-    .padEnd(20, " ")}  `,
-);
-console.log("日 月 火 水 木 金 土  ");
+    for (let i = 0; i < firstMonthDay; i++) {
+      process.stdout.write("   ");
+    }
 
-for (let i = 0; i < firstMonthDay; i++) {
-  process.stdout.write("   ");
-}
+    for (
+      let date = firstMonthDate;
+      date < lastMonthDate;
+      date.setDate(date.getDate() + 1)
+    ) {
+      const dateString = date.toLocaleDateString("ja-JP", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      });
+      const paddedDate = `${date.getDate()}`.padStart(2, " ");
+      const displayDate =
+        todayString === dateString ? chalk.bgWhite(paddedDate) : paddedDate;
 
-for (
-  let date = firstMonthDate;
-  date < lastMonthDate;
-  date.setDate(date.getDate() + 1)
-) {
-  const dateString = date.toLocaleDateString("ja-JP", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  });
-  const paddedDate = `${date.getDate()}`.padStart(2, " ");
-  const displayDate =
-    todayString === dateString ? chalk.bgWhite(paddedDate) : paddedDate;
+      if (date.getDay() === 6) {
+        console.log(`${displayDate}`);
+      } else {
+        process.stdout.write(displayDate);
+        process.stdout.write(" ");
+      }
+    }
 
-  if (date.getDay() === 6) {
-    console.log(`${displayDate}`);
-  } else {
-    process.stdout.write(displayDate);
-    process.stdout.write(" ");
-  }
-}
-
-console.log("\n");
+    console.log("\n");
