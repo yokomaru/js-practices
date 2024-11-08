638e4a12b1860e1ccfabad8b5bfd4180033e495f
yokomaru <cabochetoganlan@gmail.com>
2024-11-09 02:17:43 +0900

    addd: callback presence error
    - 一旦エラーありのコールバック処理を追加

--- 03.asynchronous/callback_no_error.js
+++ 03.asynchronous/callback_no_error.js
@@ -2,27 +2,29 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-db.run("CREATE TABLE books (title TEXT)", () => {
-  {
-
-    const stmt = db.each("INSERT INTO books VALUES (?)", () => {
-      for (let i = 1; i <= 10; i++) {
-        stmt.run("Title " + i);
-      }
-      stmt.finalize(() => {
-        db.each(
-          "SELECT rowid AS id, title FROM books",
-          (error, row) => {
-            console.log(row.id + ": " + row.title);
-          },
-          () => {
-            db.run("DROP TABLE books", () => {
-              console.log("削除");
-              db.close();
-            });
-          },
-        );
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT)",
+  () => {
+    {
+      const stmt = db.prepare("INSERT INTO books (title) VALUES (?)", () => {
+        for (let i = 1; i <= 10; i++) {
+          stmt.run("Title " + i);
+        }
+        stmt.finalize(() => {
+          db.each(
+            "SELECT id, title FROM books",
+            (error, row) => {
+              console.log(row.id + ": " + row.title);
+            },
+            () => {
+              db.run("DROP TABLE books", () => {
+                console.log("削除");
+                db.close();
+              });
+            },
+          );
+        });
       });
-    });
-  }
-});
+    }
+  },
+);
