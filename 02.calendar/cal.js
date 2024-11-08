3da0fb091102bbe69c0cbf15846f56aaf038e2b2
yokomaru <cabochetoganlan@gmail.com>
2024-11-08 23:26:31 +0900

    add: error message and return
    - エラーメッセージを表示する処理を追加
       - '-y' のみ指定して値がない場合
       - '-m' のみ指定して値がない場合
       - '-y' の指定があり、数字かつ1970~2100の範囲内でない場合
       - '-m' のみ指定して値がない場合
    - エラー時にreturn で処理を抜けるため、処理を関数に区切る
        - init() エラーチェックと引数の処理を行う
        - buildCalender() カレンダーの組み立てから表示までを行う

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,52 +3,83 @@
 import minimist from "minimist";
 import chalk from "chalk";
 
-const argv = minimist(process.argv.slice(2));
-const today = new Date();
-const year =
-  typeof argv["y"] !== "undefined" && 1970 <= argv["y"] && argv["y"] <= 2100
-    ? argv["y"]
-    : today.getFullYear();
-const month =
-  typeof argv["m"] !== "undefined" && 1 <= argv["m"] && argv["m"] <= 12
-    ? argv["m"]
-    : today.getMonth() + 1;
-const calenderTitle = `${month}月 ${year}`;
-const calenderTitleLength = calenderTitle.length;
+function init() {
+  const argv = minimist(process.argv.slice(2));
+  const today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
 
-console.log(
-  `${calenderTitle
-    .padStart(
-      calenderTitleLength + Math.floor((20 - calenderTitleLength) / 2),
-      " ",
-    )
-    .padEnd(20, " ")}`,
-);
+  if (typeof argv["y"] === "boolean")
+    return console.error("cal: option requires an argument -- y");
 
-console.log("日 月 火 水 木 金 土");
+  if (typeof argv["m"] === "boolean")
+    return console.error("cal: option requires an argument -- m");
 
-const firstMonthDate = new Date(year, month - 1, 1);
-
-for (let i = 0; i < new Date(firstMonthDate).getDay(); i++) {
-  process.stdout.write("   ");
-}
-
-for (
-  let date = firstMonthDate;
-  date < new Date(year, month, 1, 0);
-  date.setDate(date.getDate() + 1)
-) {
-  const displayDate =
-    today.toDateString() === date.toDateString()
-      ? chalk.bgWhite(`${date.getDate()}`.padStart(2, " "))
-      : `${date.getDate()}`.padStart(2, " ");
-
-  if (date.getDay() === 6) {
-    console.log(displayDate);
-  } else {
-    process.stdout.write(displayDate);
-    process.stdout.write(" ");
+  if (typeof argv["y"] !== "undefined") {
+    if (
+      typeof argv["y"] === "number" &&
+      1970 <= argv["y"] &&
+      argv["y"] <= 2100
+    ) {
+      year = argv["y"];
+    } else {
+      return console.error(`cal: year \`${argv["y"]}' not in range 1970..2100`);
+    }
   }
+
+  if (typeof argv["m"] !== "undefined") {
+    if (typeof argv["m"] === "number" && 1 <= argv["m"] && argv["m"] <= 12) {
+      month = argv["m"];
+    } else {
+      return console.error(
+        `cal: ${argv["m"]} is neither a month number (1..12) nor a name`,
+      );
+    }
+  }
+
+  buildCalender(year, month, today);
 }
 
-console.log();
+function buildCalender(year, month, today) {
+  const calenderTitle = `${month}月 ${year}`;
+  const calenderTitleLength = calenderTitle.length;
+
+  console.log(
+    `${calenderTitle
+      .padStart(
+        calenderTitleLength + Math.floor((20 - calenderTitleLength) / 2),
+        " ",
+      )
+      .padEnd(20, " ")}`,
+  );
+
+  console.log("日 月 火 水 木 金 土");
+
+  const firstMonthDate = new Date(year, month - 1, 1);
+
+  for (let i = 0; i < new Date(firstMonthDate).getDay(); i++) {
+    process.stdout.write("   ");
+  }
+
+  for (
+    let date = firstMonthDate;
+    date < new Date(year, month, 1, 0);
+    date.setDate(date.getDate() + 1)
+  ) {
+    const displayDate =
+      today.toDateString() === date.toDateString()
+        ? chalk.bgWhite(`${date.getDate()}`.padStart(2, " "))
+        : `${date.getDate()}`.padStart(2, " ");
+
+    if (date.getDay() === 6) {
+      console.log(displayDate);
+    } else {
+      process.stdout.write(displayDate);
+      process.stdout.write(" ");
+    }
+  }
+
+  console.log();
+}
+
+init();
