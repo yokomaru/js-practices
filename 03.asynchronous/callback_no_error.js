20a572992c3e382ad6195f870aa4515621932077
yokomaru <cabochetoganlan@gmail.com>
2024-11-10 00:01:54 +0900

    fix: eachの部分を修正

--- 03.asynchronous/callback_no_error.js
+++ 03.asynchronous/callback_no_error.js
@@ -2,29 +2,22 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-db.run(
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT)",
-  () => {
-    {
-      const stmt = db.prepare("INSERT INTO books (title) VALUES (?)", () => {
-        for (let i = 1; i <= 10; i++) {
-          stmt.run("Title " + i);
-        }
-        stmt.finalize(() => {
-          db.each(
-            "SELECT id, title FROM books",
-            (error, row) => {
-              console.log(row.id + ": " + row.title);
-            },
-            () => {
-              db.run("DROP TABLE books", () => {
-                console.log("削除");
-                db.close();
-              });
-            },
-          );
+db.run("CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT)", () => {
+  {
+    db.run(
+      "INSERT INTO books (title) values ($1)",
+      {
+        $1: "Title1",
+      },
+      function () {
+        console.log(`this.lastID: ${this.lastID}`);
+        db.get("SELECT id, title FROM books", (error, row) => {
+          console.log(row.id + ": " + row.title);
+          db.run("DROP TABLE books", () => {
+            db.close();
+          });
         });
-      });
-    }
-  },
-);
+      },
+    );
+  }
+});
