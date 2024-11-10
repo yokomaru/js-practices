08202a31e67a305525efd1cc6174103946f09aba
yokomaru <cabochetoganlan@gmail.com>
2024-11-11 00:07:30 +0900

    add: エラー処理も追加

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -1,9 +1,10 @@
-//import timers from "timers/promises";
+import timers from "timers/promises";
 import sqlite3 from "sqlite3";
 
 const create = "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT)";
-const insert = "INSERT INTO books (title) values ($1)";
+const insert = "INSERT INTO books (id, title) values ($1, $2)";
 const select = "SELECT id, title FROM books";
+const selectError = "SELECT id, titl FROM books";
 const drop = "DROP TABLE books";
 
 function dbRun(db, sql, param) {
@@ -40,7 +41,7 @@ function dbOperation() {
   const db = new sqlite3.Database(":memory:");
 
   dbRun(db, create).then(() => {
-    const param = { $1: "Title1" };
+    const param = { $2: "Title1" };
     dbRun(db, insert, param)
       .then((result) => {
         console.log(`this.lastID: ${result.lastID}`);
@@ -59,4 +60,41 @@ function dbOperation() {
   });
 }
 
+function dbErrorOperation() {
+  const db = new sqlite3.Database(":memory:");
+
+  dbRun(db, create).then(() => {
+    const param1 = { $1: 1, $2: "Title1" };
+    const param2 = { $1: 1, $2: "Title1" };
+    dbRun(db, insert, param1)
+      .then(
+        dbRun(db, insert, param2)
+          .then((result) => {
+            console.log(`this.lastID: ${result.lastID}`);
+          })
+          .catch((result) => {
+            // rejectの結果が引数に入る
+            console.log(result);
+          }),
+      )
+      .then(() => {
+        dbGet(db, selectError)
+          .then((row) => {
+            console.log(row.id + ": " + row.title);
+          })
+          .catch((result) => {
+            // rejectの結果が引数に入る
+            console.log(result);
+          })
+          .then(() => {
+            dbRun(db, drop).then(() => {
+              dbClose(db);
+            });
+          });
+      });
+  });
+}
+
 dbOperation();
+await timers.setTimeout(100);
+dbErrorOperation();
