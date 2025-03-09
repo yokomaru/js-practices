7ad4283d62051a6a1059f9d5551f67160bc95a91
yokomaru <cabochetoganlan@gmail.com>
2025-03-09 19:01:59 +0900

    fix: write SQL keyword parts in uppercase
    - insertのvaluesが小文字になっていたが、SQL のキーワード部分は大文字で書くので修正

--- 03.asynchronous/query.js
+++ 03.asynchronous/query.js
@@ -1,6 +1,6 @@
 export const createQuery =
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE)";
-export const insertQuery = "INSERT INTO books (title) values ($1)";
+export const insertQuery = "INSERT INTO books (title) VALUES ($1)";
 export const selectQuery = "SELECT id, title FROM books";
 export const errorSelectQuery = "SELECT id, titl FROM books";
 export const dropQuery = "DROP TABLE books";
