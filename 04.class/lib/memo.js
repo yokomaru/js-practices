84d4281a4bf10aec7d51b2d7534fea037163904b
yokomaru <cabochetoganlan@gmail.com>
2025-04-12 22:35:12 +0900

    fix:  name in memo to split on new line instead of slice
    - 改行が見当たらない時に-1を返すため、最後の一文字が削除されてしまうので改行文字でsplitし最初の要素を返すように修正

--- 04.class/lib/memo.js
+++ 04.class/lib/memo.js
@@ -6,7 +6,7 @@ class Memo {
   }
 
   #generateDisplayName = () => {
-    const slicedName = this.content?.slice(0, this.content?.indexOf("\n"));
+    const slicedName = this.content?.split("\n")[0];
     return !slicedName ? "No Title" : slicedName;
   };
 }
