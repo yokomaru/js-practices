de55db4e07b48bed061fbb289069269ac9396c1e
yokomaru <cabochetoganlan@gmail.com>
2025-04-12 20:32:29 +0900

    add: memo-control.js
    - 各メモの操作(index, delete, crette)を担当する
    - DBをここでは直接扱わず、Data BaseOperationに必要な値を渡しその結果を元に処理を行う

---
+++ 04.class/lib/memo-control.js
@@ -0,0 +1,31 @@
+import { Memo } from "./memo.js";
+
+class MemoControl {
+  constructor(databaseOperation) {
+    this.databaseOperation = databaseOperation;
+  }
+
+  index = async () => {
+    const query = "select * from memos";
+    const rows = await this.databaseOperation.selectAll(query);
+    if (rows.length == 0) {
+      throw new Error("No Data");
+    }
+    const memos = rows.map((value) => {
+      return new Memo(value.id, value.content);
+    });
+    return memos;
+  };
+
+  delete = async (id) => {
+    const query = "DELETE FROM memos WHERE id = ?";
+    return await this.databaseOperation.delete(query, id);
+  };
+
+  create = async (value) => {
+    const query = "insert into memos(content) values(?)";
+    return await this.databaseOperation.insert(query, value);
+  };
+}
+
+export { MemoControl };
